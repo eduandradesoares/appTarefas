ee4f2db57b017f528a2ba0fe32d6063c811a89e1
Eduardo Andrade <eduardo.soares@havas.com.br>
2021-10-04 13:28:26 -0300

    Excluindo tarefa e adicionando modal

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import AppLoading from 'expo-app-loading';
 import { AntDesign } from '@expo/vector-icons';
 import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView, Modal, TouchableHighlight, TextInput } from 'react-native';
 
 
 
 export default function App() {
 
   const image = require ('./resources/bg.jpg' );
+
+  console.disableYellowBox = true;
+  const [tarefas, setarTarefas] = useState([
+    {
+      id: 1,
+      tarefa: 'Minha tarefa 1'
+    },
+    {
+      id: 2,
+      tarefa: 'Minha outra tarefa..'
+    }
+  ]);
+
+  const [modal, setModal] = useState(true);
 
   let [fontsLoaded] = useFonts({
     Lato_400Regular,
@@ -21,23 +35,59 @@ export default function App() {
     );
   }
 
+  function deletarTarefa(id){
+    alert('Tarefa com id '+id+' foi deletada com sucesso');
+    let newTarefas = tarefas.filter(function(val){
+      return val.id != id;
+    });
+
+    setarTarefas(newTarefas);
+  }
+
   return (
     <ScrollView style={{flex:1}}> 
     <StatusBar hidden />     
+    <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modal}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <TextInput autoFocus={true}>Hello World!</TextInput>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+              onPress={() => {
+                setModal(!modal);
+              }}>
+              <Text style={styles.textStyle}>Adicionar tarefa</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
       <ImageBackground source={image} style={styles.image}>
         <View style={styles.coverView}>
           <Text style={styles.textHeader}>Lista de Tarefas</Text>
           </View>
       </ImageBackground>   
-
-      <View style={styles.tarefasSingle}>
+      {
+        tarefas.map(function(val){
+          return(<View style={styles.tarefasSingle}>
         <View style={{flex:1, width: '100%', padding:10}}>
-          <Text>Minha tarefa numero 1 do dia xxx do mês xxx</Text>
+          <Text>{val.tarefa}</Text>
         </View>
         <View style={{alignItems:'flex-end', flex:1, padding:10}}>
-          <TouchableOpacity><AntDesign name="minuscircleo" size={24} color="black" /></TouchableOpacity>
+          <TouchableOpacity onPress={()=> deletarTarefa(val.id)}><AntDesign name="minuscircleo" size={24} color="black" /></TouchableOpacity>
         </View>
-      </View>   
+      </View>);
+        })
+      
+      }
+
+      <TouchableOpacity style={styles.btnAddTarefa} onPress={()=>setModal(true)}><Text style={{textAlign: 'center', color: 'white'}}>Adicionar Tarefa!</Text></TouchableOpacity>
     </ScrollView>
   );
 }
@@ -47,6 +97,13 @@ const styles = StyleSheet.create({
     width: '100%',
     height: 80,
     resizeMode: 'cover'
+  },
+  btnAddTarefa:{
+    width: 200,
+    padding: 8,
+    backgroundColor:'gray',
+    marginTop:20
+
   },
   textHeader:{
     textAlign: 'center',
@@ -67,5 +124,43 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
     flexDirection: 'row',
     paddingBottom: 10
-  }
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    zIndex: 5
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
 });
