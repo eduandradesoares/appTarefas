90356f024c4661d4d48adc52216abb9ea3c8dd12
Eduardo Andrade <eduardo.soares@havas.com.br>
2021-10-04 12:18:28 -0300

    front end de tarefas

--- App.js
+++ App.js
@@ -1,20 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { AntDesign } from '@expo/vector-icons';
+import { useFonts, Lato_400Regular } from '@expo-google-fonts/lato';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, ScrollView } from 'react-native';
 
 
 
 export default function App() {
 
   const image = require ('./resources/bg.jpg' );
+
+  let [fontsLoaded] = useFonts({
+    Lato_400Regular,
+  });
+
+  if(!fontsLoaded){
+    return (
+    <AppLoading />
+    );
+  }
+
   return (
-    <View style={{flex:1}}>      
+    <ScrollView style={{flex:1}}> 
+    <StatusBar hidden />     
       <ImageBackground source={image} style={styles.image}>
         <View style={styles.coverView}>
           <Text style={styles.textHeader}>Lista de Tarefas</Text>
           </View>
-      </ImageBackground>      
-    </View>
+      </ImageBackground>   
+
+      <View style={styles.tarefasSingle}>
+        <View style={{flex:1, width: '100%', padding:10}}>
+          <Text>Minha tarefa numero 1 do dia xxx do mês xxx</Text>
+        </View>
+        <View style={{alignItems:'flex-end', flex:1, padding:10}}>
+          <TouchableOpacity><AntDesign name="minuscircleo" size={24} color="black" /></TouchableOpacity>
+        </View>
+      </View>   
+    </ScrollView>
   );
 }
 
@@ -28,11 +52,20 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: 'white',
     fontSize:20,
-    marginTop:20
+    marginTop:20,
+    fontFamily: 'Lato_400Regular'
   },
   coverView:{
     width: '100%',
     height: 80,
     backgroundColor: 'rgba(0,0,0,0.5)'
+  },
+  tarefasSingle: {
+    marginTop: 30,
+    width: '100%',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
+    flexDirection: 'row',
+    paddingBottom: 10
   }
 });
